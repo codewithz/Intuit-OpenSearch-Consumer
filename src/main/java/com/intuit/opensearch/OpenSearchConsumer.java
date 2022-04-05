5e4a4a89e84cb5a6b9256c8468c492999b9e3770
Code With Z <codewithz21@gmail.com>
2022-04-05 15:49:48 +0530

    Consumer Connected

--- src/main/java/com/intuit/opensearch/OpenSearchConsumer.java
+++ src/main/java/com/intuit/opensearch/OpenSearchConsumer.java
@@ -6,18 +6,31 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.opensearch.action.admin.indices.create.CreateIndexRequest;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
+
+
 
     public static RestHighLevelClient createOpenSearchClient() {
        String connString="https://m7852y50gz:e7e0v26rj4@kafka-es-cluster-5829139906.us-east-1.bonsaisearch.net:443";
@@ -50,6 +63,23 @@ public class OpenSearchConsumer {
         return restHighLevelClient;
     }
 
+    public static KafkaConsumer<String,String> createKafkaConsumer(){
+        String bootstrapServer="localhost:9092";
+        String groupId="opensearch-consumer-group";
+        //Create Consumer Configs
+        Properties properties=new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest"); // earliest/latest/none
+
+        //Create a Consumer
+        KafkaConsumer<String,String> consumer=new KafkaConsumer<String, String>(properties);
+
+        return  consumer;
+    }
+
     public static void main(String[] args) throws IOException {
 
         Logger logger= LoggerFactory.getLogger(OpenSearchConsumer.class.getName());
@@ -76,10 +106,33 @@ public class OpenSearchConsumer {
         }
 
 
-        //2. Create Kafka Consumer Clien
+        //2. Create Kafka Consumer Client
+
+        KafkaConsumer<String,String> consumer=createKafkaConsumer();
         //3. Subscribe the consumer to topic
+        consumer.subscribe(Collections.singleton("wikimedia.changes"));
         //4. Main Code Logic
+
+        while(true){
+            ConsumerRecords<String,String> records=consumer.poll(Duration.ofMillis(3000));
+
+            int recordCount=records.count();
+            logger.info("RECIEVED "+recordCount+" records(s)");
+
+            for(ConsumerRecord<String,String> record:records){
+                //Send the Record to OpenSearch
+
+                IndexRequest indexRequest=new IndexRequest("wikimedia")
+                        .source(record.value(), XContentType.JSON);
+
+                IndexResponse response=openSearchClient.index(indexRequest,RequestOptions.DEFAULT);
+
+                logger.info("Inserted 1 document in OpenSearch:"+response.getId());
+            }
+
+        }
+
         //5. Close the things
-        openSearchClient.close();
+     //   openSearchClient.close();
     }
 }
